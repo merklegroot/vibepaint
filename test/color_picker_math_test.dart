63c49a27f23db328a51f506c1ac822e7d522352c
Merkle Groot <merklegroot@gmail.com>
2026-07-08 00:54:24 -0400

    test fix

--- test/color_picker_math_test.dart
+++ test/color_picker_math_test.dart
@@ -4,21 +4,21 @@ import 'package:vibepaint/utils/color_picker_math.dart';
 
 void main() {
   test('colorToHexRgba encodes opaque color', () {
-    const color = Color(0xFFE9B5A3FF);
+    const color = Color(0xE9B5A3FF);
     expect(colorToHexRgba(color), 'E9B5A3FF');
   });
 
   test('colorFromHexRgba parses 8-digit hex', () {
     expect(
       colorFromHexRgba('E9B5A3FF'),
-      const Color(0xFFE9B5A3FF),
+      const Color(0xE9B5A3FF),
     );
   });
 
   test('colorFromHexRgba accepts 6-digit hex as opaque', () {
     expect(
       colorFromHexRgba('#E9B5A3'),
-      const Color(0xFFE9B5A3FF),
+      const Color(0xE9B5A3FF),
     );
   });
 
